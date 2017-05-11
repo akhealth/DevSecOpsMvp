5930835e5c06c77262482c5e3fb62ad1d1cec66a
ctro <clint@ctro.net>
2017-05-11 16:58:11 -0600

    Azure CLI 2.0 for creating a VM in Azure. Pretty equivalent to the powershell method

--- powershell/create-vm.bash
+++ powershell/create-vm.bash
@@ -1,8 +1,15 @@
 location="West US 2"
+group="dotnettest"
+vmname="testVM"
 
-az group create --location "$location" --name dotnettest
+az group create --location "$location" --name $group 
 
 # Destroy the resource group to quickly delete all contained resources
-# az group delete --name dotnettest
+# az group delete --name $group
+
+az vm create --resource-group $group --name $vmname \
+    --image win2016datacenter --admin-username <changeme> --admin-password <changeme> \
+    --size Standard_A1_v2 --storage-sku Standard_LRS
+
 
 
