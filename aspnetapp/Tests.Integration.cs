101adbbdb94a5afe65cbac5f692c25468151cc65
ctro <clint@ctro.net>
2017-05-15 11:16:32 -0600

    Not-working integration tests

---
+++ aspnetapp/Tests.Integration.cs
@@ -0,0 +1,37 @@
+// //using Microsoft.VisualStudio.TestTools.UnitTesting;
+// //using Microsoft.AspNetCore.Mvc;
+// using aspnetapp.Controllers;
+// //using System;
+// //using Xunit;
+
+// using System;
+// using System.Collections.Generic;
+// using System.Net;
+// using System.Net.Http;
+// using System.Threading.Tasks;
+// using Xunit;
+// using Microsoft.AspNetCore.TestHost;
+
+// // This is an example of Unit and Integration tests using the xUnit framework
+
+// namespace AspNetAppTests.Controllers.Integration
+// {
+
+//     public class HomeControllerTests
+//     {
+
+//         // Integration test, including a simple HTTP Client
+//         private readonly HttpClient _client;
+//         public HomeControllerTests()        {
+//             _client = new TestServer(TestServer.CreateBuilder().UseStartup<Startup>());
+//         }
+
+//         [Fact]
+//         public async Task TestTask()
+//         {
+//                var session = Startup.GetTestSession();
+//         }
+
+
+//     }
+// }
