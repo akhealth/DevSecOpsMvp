797fa68248f55bc4cae9bf769d33969b342cd042
ctro <clint@ctro.net>
2017-05-17 08:59:05 -0600

    DSC to enable IIS & make this a web server

--- dsc/webserver.ps1
+++ dsc/webserver.ps1
@@ -3,11 +3,19 @@
 configuration WebServer {
   Node "localhost"
   {
+    # This example creates a folder
     File FileDemo {
       Type = 'Directory'
       DestinationPath = 'C:\AKWASHERE'
       Ensure = "Present"
     }
+
+    # Enable IIS web sever
+    WindowsFeature InstallWebServer
+		{
+			Ensure = "Present"
+			Name = "Web-Server"
+		}
   }
 }
 
