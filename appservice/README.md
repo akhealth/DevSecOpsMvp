705a643b9501c3de8e0e66acbb7c2d2a9170978f
ctro <clint@ctro.net>
2017-05-24 08:22:32 -0600

    variables for create-app-service script, some doc updates.

--- appservice/README.md
+++ appservice/README.md
@@ -13,6 +13,12 @@ I've also included the raw ARM that specifies our current App-service in `appser
 
 As usual, the `az` cli is lightest-weight solution.  This might be OK if we don't end up needing to automate the creation of these app services.
 
+Customize and load your ENV
+```sh
+cp .env.bash.example .env.bash
+source .env.bash
+```
+
 See `create-app-service.bash` to create the AppService, and for an example of deploying to the service using `git push`
 
 
--- appservice/create-app-service.bash
+++ appservice/create-app-service.bash
@@ -7,10 +7,6 @@ num_workers="1"
 plan_name="DevOpsMVP-free"
 tier="F1"
 
-# Username / password for deployment
-deploy_user="changeme"
-deploy_pass="changeme"
-
 # Create AppService
 az group create --name $group_name --location $location
 az appservice plan create --name $plan_name --resource-group $group_name --location $location --number-of-workers $num_workers --sku $tier
@@ -19,7 +15,7 @@ az appservice web create --name $service_name --plan $plan_name --resource-group
 # Deployment via git
 # TODO: There may be a bug in Azure here.  If the remote service does not authenticate your `deploy_user` and `deploy_pass`, then you might have to add these "Deployment Credentials" manually in the Azure portal.
 # Create a deployment user, set up deployment from a local git repo, and get the url that we should push to.
-az appservice web deployment user set --user-name $deploy_user --password $deploy_pass
+az appservice web deployment user set --user-name $DEPLOY_USER --password $DEPLOY_PASS
 url=$(az appservice web source-control config-local-git --name $service_name --resource-group $group_name --query url --output tsv)
 
 # $url will look like: https://AKAzureAdmin@devopsmvp-staging.scm.azurewebsites.net/DevOpsMVP-staging.git
