d7e582138ee64205ccdda57843a240f25b24c826
ctro <clint@ctro.net>
2017-06-01 16:09:50 -0600

    Selecting SQL Server results - hard coded connection to local Docker/SQL Server - added deps for SQLServer access - runs locally

--- aspnetapp/Controllers/HomeController.cs
+++ aspnetapp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace aspnetapp.Controllers
 {
@@ -15,8 +16,35 @@ namespace aspnetapp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
 
+            string _serverName = "localhost";
+            string _dataBaseName = "AKTestDataBase";
+            string _userID = "SA";
+            string _password = "BaldEagle123";
+            string _connectionStringTempalte = "Server=tcp:{0};Initial Catalog={1};Persist Security Info=False;User ID={2};Password={3}";
+            string connectionString = String.Format(_connectionStringTempalte, _serverName, _dataBaseName, _userID, _password);
+            string qresult = "";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    var command = new SqlCommand("SELECT * FROM Employees", connection);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            qresult += $"Employee #{reader[0]}: {reader[1]}, State: {reader[2]}\r\n";
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("ERROR: " + ex.Message);
+                }
+            }
+            
+            ViewData["Message"] = qresult; 
             return View("About");
         }
 
